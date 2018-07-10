1a44ff0aab1173b0ccf5f24b397b526943a0eabf
Kate1234567891 <katehalle617@icloud.com>
2018-07-10 15:10:32 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,7 +1,9 @@
 
 # Code your methods below
-def skip-step 
+def stance 
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
+  
+  
   puts "Lift right foot."
   puts "Return right foot to the ground."
   puts "Finishing with a small skip-step."
