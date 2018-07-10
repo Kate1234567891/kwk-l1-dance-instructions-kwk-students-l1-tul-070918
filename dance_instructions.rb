ef791b5a20afef4007a5192d2fe88deeabc8ea2c
Kate1234567891 <katehalle617@icloud.com>
2018-07-10 15:11:26 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,8 +3,11 @@
 def stance 
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
 end
+
 def base  
   puts "Lift right foot."
   puts "Return right foot to the ground."
   puts "Finishing with a small skip-step."
-  
+end
+
+def
